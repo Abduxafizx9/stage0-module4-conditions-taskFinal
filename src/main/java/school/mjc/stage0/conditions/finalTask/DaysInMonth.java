401118147b64e468df8976963dab1601fc8c4093
Abdulxafizx9 <abdulxafizx999@gmail.com>
2023-10-28 01:26:46 +0500

    done

--- src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
@@ -25,6 +25,7 @@ public class DaysInMonth {
                                      leap = false;
                                      break;
                              }
+                             break;
                          default:
                              leap = true;
                              break;
