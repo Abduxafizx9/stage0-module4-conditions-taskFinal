ffc1fd5631adf5bb154da9185f1d7f66784c6b05
Abdulxafizx9 <abdulxafizx999@gmail.com>
2023-10-28 01:03:40 +0500

    done

--- src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
@@ -1,7 +1,65 @@
 package school.mjc.stage0.conditions.finalTask;
 
 public class DaysInMonth {
+
     public void printDays(int year, int month) {
+        int remainder4,remainder100,remainder400;
+        boolean leap;
+        remainder4=year%4;
+        remainder100=year%100;
+        remainder400=year%400;
+         if(year>0 && (month>0 && month<13))
+         {
+             switch(remainder4)
+             {
+                 case 0:
+                     switch (remainder100)
+                     {
+                         case 0:
+                             switch (remainder400)
+                             {
+                                 case 0:
+                                     leap = true;
+                                     break;
+                                 default:
+                                     leap = false;
+                                     break;
+                             }
+                         default:
+                             leap = true;
+                             break;
+                     }
+                     break;
+                 default:
+                     leap = false;
+
+
+             }
+             switch(month)
+             {
+                 case 1:
+                 case 12:
+                 case 10:
+                 case 8:
+                 case 7:
+                 case 5:
+                 case 3:
+                     System.out.println("31");
+                     break;
+                 case 2:
+                     if(leap)
+                         System.out.println("29");
+                     else
+                         System.out.println("28");
+
+                 default:
+                     System.out.println("30");
+
+             }
+
+         }
+         else
+             System.out.println("invalid date");
 
     }
 }
--- src/main/java/school/mjc/stage0/conditions/finalTask/IntegerDivider.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/IntegerDivider.java
@@ -2,5 +2,18 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class IntegerDivider {
     public void printCompletelyDivided(int dividend, int divider) {
+        if(divider==0)
+            System.out.println("division by zero");
+        int result = dividend/divider;
+        int dividend1 = result*divider;
+        switch (dividend-dividend1)
+        {
+            case 0:
+                System.out.println("can be divided completely");
+                break;
+            default:
+                System.out.println("cannot be divided completely");
+
+        }
     }
 }
